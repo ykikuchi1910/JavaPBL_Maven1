65ea6963699722e4c19b32a7bf8bbb74b2898058
ykikuchi1910 <a1910yk@aiit.ac.jp>
2020-04-10 23:44:12 +0900

    Test Add

--- src/main/java/Maven1/FizzBuzz.java
+++ src/main/java/Maven1/FizzBuzz.java
@@ -1,30 +1,28 @@
 package Maven1;
-
+//
 public class FizzBuzz {
-// 3で割り切れる = Fizz
-// 5で割り切れる = Buzz
-// 3でも5でも割り切れる = FizzBuzz
     public static void main(String[] args){
         // 見出し表示の出力
         System.out.println("実行例");
-        String printout ;
+
         for(int i = 1; i <= 150; i++) {
-            // 変数printoutを初期化
-            printout = "";
-            // 3かつ5で割り切れるとき
-            if((i % 3 == 0) && (i % 5 == 0)) {
-                printout = "FizzBuzz";
-                // 3で割り切れるとき
-            } else if(i % 3 == 0) {
-                printout = "Fizz";
-                // 5で割り切れるとき
-            } else if(i % 5 == 0) {
-                printout += "Buzz";
-                // それ以外のとき
-            } else {
-                printout = "" + i;
-            }
-            System.out.println(printout);
+            System.out.println(Output(i));
+        }
+    }
+
+    public static String Output(int i){
+        // 3かつ5で割り切れるとき
+        if((i % 3 == 0) && (i % 5 == 0)) {
+            return ("FizzBuzz");
+         // 3で割り切れるとき
+        }else if(i % 3 == 0) {
+            return ("Fizz");
+         // 5で割り切れるとき
+        }else if(i % 5 == 0) {
+            return ("Buzz");
+         // それ以外のとき
+        }else {
+            return ("" + i);
         }
     }
 }
---
+++ src/test/java/Maven1/FizzBuzzTest.java
@@ -0,0 +1,42 @@
+package Maven1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz f;
+
+    @Before
+    public void init() {
+        FizzBuzz f = new FizzBuzz();
+    }
+
+    @Test
+    public void test15(){
+        FizzBuzz f = new FizzBuzz();
+        assertThat("FizzBuzz", FizzBuzz.Output(15));
+    }
+    @Test
+    public void test5(){
+        FizzBuzz f = new FizzBuzz();
+        assertThat("Buzz", FizzBuzz.Output(5));
+    }
+    @Test
+    public void test3() {
+        FizzBuzz f = new FizzBuzz();
+        assertThat("Fizz", FizzBuzz.Output(3));
+    }
+    @Test
+    public void test1() {
+        FizzBuzz f = new FizzBuzz();
+        assertThat("1", FizzBuzz.Output(1));
+    }
+    @Test
+    public void test149() {
+        FizzBuzz f = new FizzBuzz();
+        assertThat("149", FizzBuzz.Output(149));
+    }
+    private void assertThat(String fizzBuzz, String output) {
+    }
+}
