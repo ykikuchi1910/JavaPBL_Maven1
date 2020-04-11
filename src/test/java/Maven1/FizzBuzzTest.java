53412e60ebea4757c7fbfaca16014309c0040901
ykikuchi1910 <a1910yk@aiit.ac.jp>
2020-04-12 02:11:32 +0900

    @Before でインスタンス生成

--- src/test/java/Maven1/FizzBuzzTest.java
+++ src/test/java/Maven1/FizzBuzzTest.java
@@ -14,27 +14,27 @@ public class FizzBuzzTest {
 
     @Test
     public void test15(){
-        FizzBuzz f = new FizzBuzz();
+     //   FizzBuzz f = new FizzBuzz();
         assertThat("FizzBuzz", FizzBuzz.Output(15));
     }
     @Test
     public void test5(){
-        FizzBuzz f = new FizzBuzz();
+    //    FizzBuzz f = new FizzBuzz();
         assertThat("Buzz", FizzBuzz.Output(5));
     }
     @Test
     public void test3() {
-        FizzBuzz f = new FizzBuzz();
+     //   FizzBuzz f = new FizzBuzz();
         assertThat("Fizz", FizzBuzz.Output(3));
     }
     @Test
     public void test1() {
-        FizzBuzz f = new FizzBuzz();
+     //   FizzBuzz f = new FizzBuzz();
         assertThat("1", FizzBuzz.Output(1));
     }
     @Test
     public void test149() {
-        FizzBuzz f = new FizzBuzz();
+     //   FizzBuzz f = new FizzBuzz();
         assertThat("149", FizzBuzz.Output(149));
     }
     private void assertThat(String fizzBuzz, String output) {
